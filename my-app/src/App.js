87963ff9ea63fdc203086291f3e2289e216ca8ab
Zuhaib Shakeel <zuhaib.s@sibelinternational.com>
2021-04-13 15:06:42 +0900

    added syncfusion chart

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,7 +2,9 @@ import {
   Category,
   ChartComponent,
   ColumnSeries,
+  DataLabel,
   Inject,
+  Legend,
   LineSeries,
   SeriesCollectionDirective,
   SeriesDirective,
@@ -12,34 +14,46 @@ import {
 const obj = JSON.parse(
   '{ "data": { "x": ["Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sep","Oct","Nov","Dec"], "y": [35,28,34,32,50,32,35,55,38,30,25,32] }}'
 );
-const x_len = Object.keys(obj["data"]["x"]).length;
-const y_len = Object.keys(obj["data"]["y"]).length;
+const xLen = Object.keys(obj["data"]["x"]).length;
+const yLen = Object.keys(obj["data"]["y"]).length;
 let processed_data = [];
 let primaryxAxis;
+let primaryyAxis;
+let lSettings;
+let tooltipSettings;
 
-if (x_len === y_len) {
-  for (let i = 0; i < x_len; i++) {
+if (xLen === yLen) {
+  for (let i = 0; i < xLen; i++) {
     let temp_object = { x: obj["data"]["x"][i], y: obj["data"]["y"][i] };
     processed_data.push(temp_object);
   }
   primaryxAxis = { valueType: "Category" };
+  primaryyAxis = { labelFormat: "${value}K" };
+  lSettings = true;
+  tooltipSettings = { enable: true, shared: false };
 }
 function App() {
   return (
-    <div>
-      <h1>Test Chart</h1>
-      <ChartComponent id="charts" primaryXAxis={primaryxAxis}>
-        <Inject services={[ColumnSeries, Tooltip, LineSeries, Category]} />
-        <SeriesCollectionDirective>
-          <SeriesDirective
-            dataSource={processed_data}
-            xName="x"
-            yName="y"
-            name="Sales"
-          />
-        </SeriesCollectionDirective>
-      </ChartComponent>
-    </div>
+    <ChartComponent
+      id="charts"
+      primaryXAxis={primaryxAxis}
+      primaryYAxis={primaryyAxis}
+      legendSettings={lSettings}
+      title="Test Chart"
+      tooltip={tooltipSettings}
+    >
+      <Inject
+        services={[ColumnSeries, Tooltip, Legend, LineSeries, Category]}
+      />
+      <SeriesCollectionDirective>
+        <SeriesDirective
+          dataSource={processed_data}
+          xName="x"
+          yName="y"
+          name="Sales"
+        />
+      </SeriesCollectionDirective>
+    </ChartComponent>
   );
 }
 
