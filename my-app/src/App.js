f8314868e47362a08b73c50fb400c8c417af7cc4
Zuhaib Shakeel <zuhaib.s@sibelinternational.com>
2021-04-13 14:43:48 +0900

    added json parsing code

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import {
+  Category,
+  ChartComponent,
+  ColumnSeries,
+  Inject,
+  LineSeries,
+  SeriesCollectionDirective,
+  SeriesDirective,
+  Tooltip,
+} from "@syncfusion/ej2-react-charts";
 
+const obj = JSON.parse(
+  '{ "data": { "x": ["Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sep","Oct","Nov","Dec"], "y": [35,28,34,32,50,32,35,55,38,30,25,32] }}'
+);
+const x_len = Object.keys(obj["data"]["x"]).length;
+const y_len = Object.keys(obj["data"]["y"]).length;
+let processed_data = [];
+let primaryxAxis;
+
+if (x_len === y_len) {
+  for (let i = 0; i < x_len; i++) {
+    let temp_object = { x: obj["data"]["x"][i], y: obj["data"]["y"][i] };
+    processed_data.push(temp_object);
+  }
+  primaryxAxis = { valueType: "Category" };
+}
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Test Chart</h1>
+      <ChartComponent id="charts" primaryXAxis={primaryxAxis}>
+        <Inject services={[ColumnSeries, Tooltip, LineSeries, Category]} />
+        <SeriesCollectionDirective>
+          <SeriesDirective
+            dataSource={processed_data}
+            xName="x"
+            yName="y"
+            name="Sales"
+          />
+        </SeriesCollectionDirective>
+      </ChartComponent>
     </div>
   );
 }
